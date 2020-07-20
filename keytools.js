cce2f0d9c8427c01cc97ffdda1443d120acd2b81
sebaLitter26 <sebastian_litter@hotmail.com>
2020-07-20 20:13:57 -0300

    last commit

--- keytools.js
+++ keytools.js
@@ -1,16 +1,30 @@
 var intermitencia_carrusel = false;
+var switch_author = false;
+var switch_paragraph = false;
 
-function show_paragraph(i){
+function show_author(){
+  
+  let deg = (switch_author) ? 0:180;
+  document.getElementById("paragraph_author_expand").style.webkitTransform = 'rotate('+deg+'deg)';
+  document.getElementsByClassName("paragraph_author")[0].scroll({ top: (deg*10), behavior: 'smooth' });
+  switch_author=!switch_author;
+}
+
+function show_paragraph(){
     
-    //this.classList.toggle("active");
-    var content = document.getElementsByClassName("paragraph_hided")[i];
-    console.log(i,content);
+    
+    var content = document.getElementsByClassName('paragraph_hided')[0];
+    let deg = (switch_paragraph) ? 0:270;
+    switch_paragraph=!switch_paragraph;
+    document.getElementById("explodeButton").style.webkitTransform = 'rotate('+deg+'deg)';
     console.log("display",content);
+
     if (content.style.display === "block") {
       content.style.display = "none";
     } else {
       content.style.display = "block";
     }
+    
 }
 
 function elementInViewport(el) {
@@ -39,10 +53,11 @@ function elementInViewport(el) {
 window.onload = function () {
 
     const TIEMPO_INTERVALO_MILESIMAS_SEG = 3000;
-    let posicionActual = 0;
+    
     let $botonStop = document.getElementsByClassName('carousel')[0];
     var liEls = document.querySelectorAll('.carousel p');
     let cant_comments = liEls.length ;
+    let posicionActual = cant_comments;
     let intervalo;
 
     // Funciones
@@ -54,8 +69,9 @@ window.onload = function () {
 
       if(!this.elementInViewport($botonStop)) return;
 
-        posicionActual++;
-        posicionActual=posicionActual%cant_comments;
+        posicionActual--;
+        if(posicionActual<0)
+          posicionActual= cant_comments;
 
         liEls[posicionActual].scrollIntoView({block: 'nearest', behavior: 'smooth'});
     }
