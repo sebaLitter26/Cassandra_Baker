b234bc8e0bddab45b347325d334558f29507ebe4
sebaLitter26 <sebastian_litter@hotmail.com>
2020-07-31 20:28:24 -0300

    mobile

--- mobile/keytools.js
+++ mobile/keytools.js
@@ -1,50 +1,5 @@
-var intermitencia_carrusel = false;
-var switch_author = false;
-var switch_paragraph = false;
 
-function show_author(){
-  
-  let deg = (switch_author) ? 0:180;
-  document.getElementById("paragraph_author_expand").style.webkitTransform = 'rotate('+deg+'deg)';
-  document.getElementsByClassName("paragraph_author")[0].scroll({ top: (deg*10), behavior: 'smooth' });
-  switch_author=!switch_author;
-}
-
-function show_paragraph(){
-    
-    
-    var content = document.getElementsByClassName('paragraph_hided')[0];
-    let deg = (switch_paragraph) ? 0:270;
-    switch_paragraph=!switch_paragraph;
-    document.getElementById("explodeButton").style.webkitTransform = 'rotate('+deg+'deg)';
-
-    if (content.style.display === "block") {
-      content.style.display = "none";
-    } else {
-      content.style.display = "block";
-    }
-    
-}
-
-function elementInViewport(el) {
-  var top = el.offsetTop;
-  var left = el.offsetLeft;
-  var width = el.offsetWidth;
-  var height = el.offsetHeight;
-
-  while(el.offsetParent) {
-    el = el.offsetParent;
-    top += el.offsetTop;
-    left += el.offsetLeft;
-  }
-
-  return (
-    top >= window.pageYOffset &&
-    left >= window.pageXOffset &&
-    (top + height) <= (window.pageYOffset + window.innerHeight) &&
-    (left + width) <= (window.pageXOffset + window.innerWidth)
-  );
-}
+var switch_home = false;
 
 
 
@@ -57,17 +12,50 @@ function isDesktop(event){
   
 }
 
+var pages= [{name:'menu',title:'Menu'}, {name:'home',title:'Home'},{name:'author',title:'The Author'},{name:'book',title:'The Book'},{name:'iframe',title:'Ancillary Material'},{name:'purchase',title:'Where to Purchase'},{name:'contact',title:'Contact'}];
 
 function displayPages(div_name){
   
     let pagina;
+    
 
-    let pages= ['menu', 'home','author','book','iframe','purchase','contact'];
-    pages.forEach(element=>{
-      pagina = document.querySelectorAll('.body .'+ element)[0];
-      console.log(pagina);
-      pagina.hidden = (div_name!=element);
+    this.pages.forEach(element=>{
+      pagina = document.querySelectorAll('.body .'+ element.name)[0];
+      
+      pagina.hidden = (div_name!=element.name);
+      if(div_name==element.name)  
+        this.change_title(element);
+
     });
+
+    
+}
+
+
+function toggle_menu(){
+  
+  if(switch_home){
+    
+    this.displayPages('home');
+  }else{
+    
+    this.displayPages('menu');
+  }
+  switch_home =!switch_home;
+}
+
+
+function change_title(elem_head){
+ 
+  if(elem_head.name!='home' && elem_head.name!='menu'){
+    document.getElementsByClassName('menu')[0].hidden=true;
+    let head = document.getElementsByClassName('page')[0];
+    head.hidden=(elem_head.name=='menu');
+    head.querySelector('h3').innerHTML=elem_head.title;
+  }else{
+    document.getElementsByClassName('menu')[0].hidden=false;
+    document.getElementsByClassName('page')[0].hidden=true;
+  }
     
 }
 
